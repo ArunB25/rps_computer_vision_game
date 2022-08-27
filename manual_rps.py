3d5c225c322d3e07e69f64d7585921fd0844a983
Arun <arun.jb25@hotmail.co.uk>
2022-08-27 14:21:49 +0100

    manual version of rock paper scissors created

--- manual_rps.py
+++ manual_rps.py
@@ -1,43 +1,3 @@
-# Computer_vision
-Rock Paper scissors game using computer vision
-
-Image project model created on teachable machine, to regonise 4 different classes, rock,paper,scissors and nothing. Model is on the keras_model.h5 file
-code snippets to use model (from Teachable Machine)
-
-```python """
-from keras.models import load_model
-from PIL import Image, ImageOps
-import numpy as np
-
-# Load the model
-model = load_model('keras_model.h5')
-
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-# Replace this with the path to your image
-image = Image.open('<IMAGE_PATH>')
-#resize the image to a 224x224 with the same strategy as in TM2:
-#resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-#turn the image into a numpy array
-image_array = np.asarray(image)
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-# Load the image into the array
-data[0] = normalized_image_array
-
-# run the inference
-prediction = model.predict(data)
-print(prediction)
-"""
-```
-Manual version of Rock-Paper-Scissors created, which uses python random module to make computers decision and takes user input.
-
-'''python """
 import random
 
 def play():
@@ -85,4 +45,3 @@ def play():
 if __name__ == '__main__':
 
     print(play())
-""" '''
