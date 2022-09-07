22ae7eb0d349bd84c35ca32fa73f1e27992812aa
Arun <arun.jb25@hotmail.co.uk>
2022-09-07 17:21:32 +0100

    script converted to use classes and paritally tideyd up

--- camera_alwayson_rps.py
+++ camera_alwayson_rps.py
@@ -7,179 +7,214 @@ import numpy as np
 import time
 import random
 
+class play_rps:
 
+    def get_computer_choice(self):
+                '''
+                Returns random value of rock, paper or scissors
+                '''
+                options = ["rock","paper","scissors"]
+                self.computer_choice = random.choice(options)
 
-def play():
-
-    def get_computer_choice():
+    def get_winner(self):
             '''
-            Returns random value of rock, paper or scissors
+            Returns the winner of Rock,Paper,Scissors from the Computer and User choices
             '''
-            options = ["rock","paper","scissors"]
-            return random.choice(options)
+            if self.computer_choice == "rock" and self.user_choice == "paper":
+                return("user")
+            elif self.computer_choice == "rock" and self.user_choice == "scissors":
+                return("computer")
+            elif self.computer_choice == "paper" and self.user_choice == "scissors":
+                return("user")
+            elif self.computer_choice == "paper" and self.user_choice == "rock":
+                return("computer")
+            elif self.computer_choice == "scissors" and self.user_choice == "rock":
+                return("user")
+            elif self.computer_choice == "scissors" and self.user_choice == "paper":
+                return("computer")
+            elif self.computer_choice == self.user_choice:
+                return("tie")
+            else:
+                return("Error in user input")
 
-
-    def get_winner(computer_choice, user_choice):
+    def __init__(self):
         '''
-        Returns the winner of Rock,Paper,Scissors from the Computer and User choices
+        setup the initial functions for the game
         '''
-        if computer_choice == "rock" and user_choice == "paper":
-            return("user")
-        elif computer_choice == "rock" and user_choice == "scissors":
-            return("computer")
-        elif computer_choice == "paper" and user_choice == "scissors":
-            return("user")
-        elif computer_choice == "paper" and user_choice == "rock":
-            return("computer")
-        elif computer_choice == "scissors" and user_choice == "rock":
-            return("user")
-        elif computer_choice == "scissors" and user_choice == "paper":
-            return("computer")
-        elif computer_choice == user_choice:
-            return("tie")
-        else:
-            return("Error in user input")
+        self.model = load_model('keras_model.h5')
+        self.cap = cv2.VideoCapture(0)
+        self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        self.user_wins = 0
+        self.computer_wins = 0
+        self.game_stage = 5
+        self.prompt_string = "Start Game Press S"
+        self.quit_print = "Press Q to quit"
+        self.winner_string = ""
+        self.user_choice_string = ""
+        self.computer_choice_string = ""
 
-    #Game Initialisation
-    model = load_model('keras_model.h5')
-    cap = cv2.VideoCapture(0)
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    user_wins = 0
-    computer_wins = 0
-    game_stage = 5
-    prompt_string = "Start Game Press S"
-    quit_print = "Press Q to quit"
-    winner_string = ""
-    user_guess_string = ""
-    computer_guess_string = ""
-
-    while True: 
-        ret, frame = cap.read()
-        frame = cv2.flip(frame,1) # mirror image
-        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    def video_prediction(self):
+        '''
+        get image from camera, formats image, then passes it through the ML model to get predictions and selects highest probability
+        
+        '''
+        ret, frame = self.cap.read()
+        self.frame = cv2.flip(frame,1) # mirror image
+        resized_frame = cv2.resize(self.frame, (224, 224), interpolation = cv2.INTER_AREA)
         image_np = np.array(resized_frame)
         normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-        data[0] = normalized_image
-        prediction = model.predict(data)
-        
+        self.data[0] = normalized_image
+        prediction = self.model.predict(self.data)
         rps_prediction = np.argmax(prediction)
 
         if rps_prediction == 0:
-            current_guess = "rock"  
+            self.current_choice = "rock"  
         elif rps_prediction == 1:
-            current_guess = "paper"
+            self.current_choice = "paper"
         elif rps_prediction == 2:
-            current_guess = "scissors"
+            self.current_choice = "scissors"
         elif rps_prediction == 3:
-            current_guess = "nothing"
+            self.current_choice = "nothing"
+
+    def video_overlay(self):
+        """
+        overlays text on to the current frame and displays it
+        """
         font = cv2.FONT_HERSHEY_SIMPLEX
-        current_guess_string = "Current guess: {}".format(current_guess)
-        cv2.putText(frame,current_guess_string,(30,60), font, 0.6, (10, 10, 200), 1, cv2.LINE_AA)
-        cv2.putText(frame,prompt_string,(30,40), font, 1, (10, 10, 10), 4, cv2.LINE_AA)
-        cv2.putText(frame,quit_print,(535,10), font, 0.4, (10, 10, 10), 1, cv2.LINE_AA)
-        cv2.putText(frame,winner_string,(30,470), font, 0.8, (10, 10, 10), 2, cv2.LINE_AA)
-        cv2.putText(frame,user_guess_string,(30,420), font, 0.7, (10, 10, 10), 2, cv2.LINE_AA)
-        cv2.putText(frame,computer_guess_string,(30,445), font, 0.7, (10, 10, 10), 2, cv2.LINE_AA)
+        current_choice_string = "Current choice: {}".format(self.current_choice)
+        cv2.putText(self.frame,current_choice_string,(30,60), font, 0.6, (10, 10, 200), 1, cv2.LINE_AA)
+        cv2.putText(self.frame,self.prompt_string,(30,40), font, 1, (10, 10, 10), 4, cv2.LINE_AA)
+        cv2.putText(self.frame,self.quit_print,(535,10), font, 0.4, (10, 10, 10), 1, cv2.LINE_AA)
+        cv2.putText(self.frame,self.winner_string,(30,470), font, 0.8, (10, 10, 10), 2, cv2.LINE_AA)
+        cv2.putText(self.frame,self.user_choice_string,(30,420), font, 0.7, (10, 10, 10), 2, cv2.LINE_AA)
+        cv2.putText(self.frame,self.computer_choice_string,(30,445), font, 0.7, (10, 10, 10), 2, cv2.LINE_AA)
+        cv2.imshow('frame', self.frame)
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):   # Press q to close the window
-            break
-
-       
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        if user_wins >= 3 or computer_wins >= 3:
-            game_stage = 7
-   
-        if game_stage == 1:
-            start_time = time.time()
+    def showhand_countdown(self):
+        """
+        countdown from 3 whilst updating video frames and overlays
+        """
+        start_time = time.time()
+        current_time = time.time()
+        self.prompt_string = "Show hand in 3"
+        
+        while True:
             current_time = time.time()
-            prompt_string = "Show hand in 3"
-            game_stage = 2
-        elif game_stage == 2:
-            current_time = time.time()
+            self.video_prediction()
+            self.video_overlay()
+
             if current_time >= start_time +1 and current_time <= start_time +2 :
-                prompt_string = "Show hand in 2"    
+                    self.prompt_string = "Show hand in 2"    
             elif current_time >= start_time + 2 and current_time <= start_time +3:
-                prompt_string = "Show hand in 1"
+                    self.prompt_string = "Show hand in 1"
             elif current_time >= start_time + 3:
-                game_stage = 3    
-        elif game_stage == 3:
-            prompt_string = "Show hand"
-            computer_guess = get_computer_choice()
-            user_guess = current_guess
-            if user_guess != "nothing":
-                game_stage = 4   
-        elif game_stage == 4:
-            winner = get_winner(computer_guess, user_guess)
-            user_guess_string = "User guessed: {}".format(user_guess)
-            computer_guess_string = "Computer guessed: {}".format(computer_guess)
-            if winner == "user":
-                user_wins += 1
-                winner_string = "User won! Score User {} | Computer {} ".format(user_wins, computer_wins)
-                game_stage = 5
-            elif winner == "computer":
-                computer_wins += 1
-                winner_string = "Computer won! Score User {} | Computer {} ".format(user_wins, computer_wins)
-                game_stage = 5
-            elif winner == "tie":
-                winner_string = "Tie! Score User {} | Computer {} ".format(user_wins, computer_wins)
-                game_stage = 5
-        elif game_stage == 5:
-            start_time = time.time()
-            current_time = time.time()
-            prompt_string = "Next round in 5"
-            game_stage = 6
-        elif game_stage == 6:
-            current_time = time.time()
-            if current_time >= start_time +1 and current_time <= start_time +2 :
-                prompt_string = "Next round in 4"    
-            elif current_time >= start_time + 2 and current_time <= start_time +3:
-                prompt_string = "Next round in 3"
-            elif current_time >= start_time + 3 and current_time <= start_time +4:
-                prompt_string = "Next round in 2"
-            elif current_time >= start_time + 4 and current_time <= start_time +5:
-                prompt_string = "Next round in 1"
-                user_guess_string = ""
-                computer_guess_string = ""
-            elif current_time >= start_time + 5:
-                game_stage = 1 
-        elif game_stage == 7:
-            start_time = time.time()
-            current_time = time.time()
-            if user_wins == 3:
-                winner_string = "User Won Overall!"
-            elif computer_wins == 3:
-                winner_string = "Computer Won Overall"
-            prompt_string = "Game Closes in 5"
-            user_wins = 0
-            computer_wins = 0
-            game_stage = 8
-        elif game_stage == 8:
-            current_time = time.time()
-            if current_time >= start_time +1 and current_time <= start_time +2 :
-                prompt_string = "Game Closes in 4"  
-                user_guess_string = ""
-                computer_guess_string = ""  
-            elif current_time >= start_time + 2 and current_time <= start_time +3:
-                prompt_string = "Game Closes in 3"
-            elif current_time >= start_time + 3 and current_time <= start_time +4:
-                prompt_string = "Game Closes in 2"
-            elif current_time >= start_time + 4 and current_time <= start_time +5:
-                prompt_string = "Game Closes in 1"
-            elif current_time >= start_time + 5:
-                break
-    
+                    break   
+            
+        self.prompt_string = "Show hand"
 
-         
-    # After the loop release the cap object
-    cap.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
+
+    def play(self):
+
+        while True: 
+            
+            self.video_prediction()
+            self.video_overlay()
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):   # Press q to close the window
+                break
+
+            if self.user_wins >= 3 or self.computer_wins >= 3:
+                self.game_stage = 7
+    
+            if self.game_stage == 1:
+                start_time = time.time()
+                current_time = time.time()
+                self.prompt_string = "Show hand in 3"
+                self.game_stage = 2
+            elif self.game_stage == 2:
+                current_time = time.time()
+                if current_time >= start_time +1 and current_time <= start_time +2 :
+                    self.prompt_string = "Show hand in 2"    
+                elif current_time >= start_time + 2 and current_time <= start_time +3:
+                    self.prompt_string = "Show hand in 1"
+                elif current_time >= start_time + 3:
+                    self.game_stage = 3    
+            elif self.game_stage == 3:
+                self.prompt_string = "Show hand"
+                self.get_computer_choice()
+                self.user_choice = self.current_choice
+                if self.user_choice != "nothing":
+                    self.game_stage = 4   
+            elif self.game_stage == 4:
+                winner = self.get_winner()
+                self.user_choice_string = "User chose: {}".format(self.user_choice)
+                self.computer_choice_string = "Computer chose: {}".format(self.computer_choice)
+                if winner == "user":
+                    self.user_wins += 1
+                    self.winner_string = "User won! Score User {} | Computer {} ".format(self.user_wins, self.computer_wins)
+                    self.game_stage = 5
+                elif winner == "computer":
+                    self.computer_wins += 1
+                    self.winner_string = "Computer won! Score User {} | Computer {} ".format(self.user_wins, self.computer_wins)
+                    self.game_stage = 5
+                elif winner == "tie":
+                    self.winner_string = "Tie! Score User {} | Computer {} ".format(self.user_wins, self.computer_wins)
+                    self.game_stage = 5
+            elif self.game_stage == 5:
+                start_time = time.time()
+                current_time = time.time()
+                self.prompt_string = "Next round in 5"
+                self.game_stage = 6
+            elif self.game_stage == 6:
+                current_time = time.time()
+                if current_time >= start_time +1 and current_time <= start_time +2 :
+                    self.prompt_string = "Next round in 4"    
+                elif current_time >= start_time + 2 and current_time <= start_time +3:
+                    self.prompt_string = "Next round in 3"
+                elif current_time >= start_time + 3 and current_time <= start_time +4:
+                    self.prompt_string = "Next round in 2"
+                elif current_time >= start_time + 4 and current_time <= start_time +5:
+                    self.prompt_string = "Next round in 1"
+                    self.user_choice_string = ""
+                    self.computer_choice_string = ""
+                elif current_time >= start_time + 5:
+                    self.game_stage = 1 
+            elif self.game_stage == 7:
+                start_time = time.time()
+                current_time = time.time()
+                if self.user_wins == 3:
+                    self.winner_string = "User Won Overall!"
+                elif self.computer_wins == 3:
+                    self.winner_string = "Computer Won Overall"
+                self.prompt_string = "Game Closes in 5"
+                self.user_wins = 0
+                self.computer_wins = 0
+                self.game_stage = 8
+            elif self.game_stage == 8:
+                current_time = time.time()
+                if current_time >= start_time +1 and current_time <= start_time +2 :
+                    self.prompt_string = "Game Closes in 4"  
+                    self.user_choice_string = ""
+                    self.computer_choice_string = ""  
+                elif current_time >= start_time + 2 and current_time <= start_time +3:
+                    self.prompt_string = "Game Closes in 3"
+                elif current_time >= start_time + 3 and current_time <= start_time +4:
+                    self.prompt_string = "Game Closes in 2"
+                elif current_time >= start_time + 4 and current_time <= start_time +5:
+                    self.prompt_string = "Game Closes in 1"
+                elif current_time >= start_time + 5:
+                    break
+        
+
+            
+        # After the loop release the cap object
+        self.cap.release()
+        # Destroy all the windows
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    play()
+    mygame = play_rps()
+    mygame.play()
    
     
